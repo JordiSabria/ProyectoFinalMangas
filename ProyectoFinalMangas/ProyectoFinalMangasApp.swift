2b9e237f075e6b6fb25c01fbda3338a4720e9b2a
Jordi Sabrià Pagès <jordisabria@hotmail.com>
2024-01-03 08:13:46 +0100

    Initial Commit

---
+++ ProyectoFinalMangas/ProyectoFinalMangasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoFinalMangasApp.swift
+//  ProyectoFinalMangas
+//
+//  Created by Jordi Sabrià Pagès on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoFinalMangasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
